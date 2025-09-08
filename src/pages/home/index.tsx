75f76d472ded0b25cda98fe9a476b2c699f3f76d
zhangqian <wb_zhangqian13@meituan.com>
2025-09-08 19:26:57 +0800

    feat: pull down refresh

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,5 @@
-import { View, Text } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
+import { View, Button } from '@tarojs/components'
+import Taro, { useLoad, usePullDownRefresh } from '@tarojs/taro'
 import './index.less'
 
 export default function Index () {
@@ -7,9 +7,26 @@ export default function Index () {
     console.log('Page loaded.')
   })
 
+  usePullDownRefresh(() => {
+    console.log('Page refreshed.')
+    setTimeout(() => {
+      console.log('Page stop refreshed.')
+      Taro.stopPullDownRefresh()
+    }, 1000)
+  })
+
+  const showToast = () => {
+    Taro.showToast({
+      title: 'Hello, World!',
+      icon: 'none',
+      duration: 2000
+    })
+  }
+
   return (
-    <View className='index'>
-      <section ></section>
+    <View className='main'>
+      111
+      <Button onClick={showToast}>toast</Button>
     </View>
   )
 }
